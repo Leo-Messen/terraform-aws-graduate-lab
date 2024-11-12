ff581c5e78f2e2f344b0f3c482e44a186298562c
Leo Messen <leomessen@Leos-MBP.localdomain>
2024-11-12 15:54:01 +0000

    Added VPC creation terraform

--- terraform/layers/base/providers.tf
+++ terraform/layers/base/providers.tf
@@ -9,6 +9,7 @@ terraform {
 }
 
 provider "aws" {
+
   default_tags {
     tags = {
       Environment = var.environment
--- terraform/layers/base/variables.tf
+++ terraform/layers/base/variables.tf
@@ -8,3 +8,14 @@ variable "project_name" {
   type        = string
 }
 
+variable "lab-1-vpc-cidr" {
+  description = "Lab 1 VPC CIDR range"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "lab-1-vpc-name" {
+  description = "Lab 1 VPC name"
+  type        = string
+  default     = "lab-1-vpc"
+}
