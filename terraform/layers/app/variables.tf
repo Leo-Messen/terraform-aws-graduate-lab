8cac2a929f06e81f947900372a5a5ccf32e76422
Leo Messen <leomessen@Leos-MBP.localdomain>
2024-11-12 13:35:58 +0000

    Removed default_tags variable from variables.tf files

--- terraform/layers/app/variables.tf
+++ terraform/layers/app/variables.tf
@@ -1,8 +1,3 @@
-variable "default_tags" {
-  description = "Common default tags for all deployed resources"
-  type        = map(any)
-}
-
 variable "environment" {
   description = "Environment you are deploying to e.g. dev/prod"
   type        = string
