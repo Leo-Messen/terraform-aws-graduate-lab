7d5b361eb1eafecffb5c7ad1a2f1307a7c46d3d6
Leo Messen <leomessen@Leos-MacBook-Pro.local>
2024-11-14 13:27:10 +0000

    Reused locals resource name prefix for s3 bucket name

--- terraform/layers/base/s3.tf
+++ terraform/layers/base/s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "${var.project_name}-${var.environment}-webfiles"
+  bucket = "${local.resource_name_prefix}-webfiles"
   acl    = "private"
 
   control_object_ownership = true
