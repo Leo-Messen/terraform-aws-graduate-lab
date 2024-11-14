f003a5e552ae99278cb77684eba1b4d8881fc342
Leo Messen <leomessen@Leos-MacBook-Pro.local>
2024-11-14 10:27:52 +0000

    Added id and ARN of webfiles bucket to outputs

--- terraform/layers/base/outputs.tf
+++ terraform/layers/base/outputs.tf
@@ -1,3 +1,11 @@
 output "available_azs" {
   value = data.aws_availability_zones.available_azs.names
 }
+
+output "s3_webfiles_bucket_arn" {
+  value = module.s3_bucket.s3_bucket_arn
+}
+
+output "s3_webfiles_bucket_id" {
+  value = module.s3_bucket.s3_bucket_id
+}
