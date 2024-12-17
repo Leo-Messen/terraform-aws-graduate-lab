8512ab862539883096f8d099ccc5d864b5b07abe
Leo Messen <leomessen@Leos-MBP.localdomain>
2024-12-17 13:24:06 +0000

    Added CodePipeline IAM policy tf

--- terraform/layers/deploy/iam.tf
+++ terraform/layers/deploy/iam.tf
@@ -22,7 +22,35 @@ resource "aws_iam_role" "codebuild_tf_deploy" {
   assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "codepipeline_ecs_deploy_codebuild" {
+resource "aws_iam_role_policy_attachment" "codebuild_tf_deploy_attach_policy" {
   role       = aws_iam_role.codebuild_tf_deploy.name
   policy_arn = data.aws_iam_policy.AWSCodeBuildAdminAccess.arn
+}
+################################################################################
+# Codepipeline
+################################################################################
+data "aws_iam_policy_document" "codepipeline_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy" "AWSCodePipeline_FullAccess" {
+  arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
+}
+resource "aws_iam_role" "codepipeline_tf_deploy" {
+  name               = "codepipeline-${var.environment}-${var.project_name}"
+  assume_role_policy = data.aws_iam_policy_document.codepipeline_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_tf_deploy_attach_policy" {
+  role       = aws_iam_role.codepipeline_tf_deploy.name
+  policy_arn = data.aws_iam_policy.AWSCodePipeline_FullAccess.arn
 }
